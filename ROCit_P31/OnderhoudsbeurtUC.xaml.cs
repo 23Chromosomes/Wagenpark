febb10a1a6b6b4fcc276a64945404a83cb669a5e
Maurice <59695433+maurijst@users.noreply.github.com>
2021-11-09 14:09:04 +0100

    project
    project moeite shit

--- ROCit_P31/OnderhoudsbeurtUC.xaml.cs
+++ ROCit_P31/OnderhoudsbeurtUC.xaml.cs
@@ -23,68 +23,120 @@ namespace ROCit_P31
         private DataLinQDataContext db;
         LeaseAutoController leaseC;
 
-        
+
         public OnderhoudsbeurtUC(DataLinQDataContext db)
         {
             InitializeComponent();
+         
             this.db = db;
             dgBeurten.ItemsSource = db.onderhouds.ToList();
             leaseC = new LeaseAutoController(db);
-            cbxauto.ItemsSource = leaseC.autoSelect().ToList();
-            leaseC.autoSelect();
-            cbxWerkplaats.ItemsSource = db.werkplaats.ToList();
+            cbxauto.ItemsSource = db.autos.ToList();
+            cbxWerkplaats.ItemsSource= db.werkplaats.ToList();
+        }
+
+        public void refreshgrid()
+        {
+            dgBeurten.ItemsSource = db.onderhouds.ToList();
         }
 
         private void BtnOpslaanBeurt_Click(object sender, RoutedEventArgs e)
         {
-            if(tbxbeurtnaam.Text.Length == 0)
+            if (tbxbeurtnaam.Text.Length == 0)
             {
                 MessageBox.Show("Er is geen Naam ingevoerd!");
             }
             else
             {
-                if(tbxprijs.Text.Length == 0)
+                if (tbxprijs.Text.Length == 0)
                 {
                     MessageBox.Show("Er is geen prijs ingevoerd!");
                 }
                 else
                 {
-                    if(tbxomschrijving.Text.Length == 0)
+                    if (tbxomschrijving.Text.Length == 0)
                     {
                         MessageBox.Show("Er is geen omschrijving!");
                     }
                     else
                     {
-                        if(dpUitvoerdatum.Text.Length == 0)
+                        if (dpUitvoerdatum.Text.Length == 0)
                         {
                             MessageBox.Show("Er is geen uitvoerdatum geselecteerd!");
                         }
                         else
                         {
-                            if(string.IsNullOrEmpty(cbxauto.Text))
+                            if (string.IsNullOrEmpty(cbxauto.Text))
                             {
                                 MessageBox.Show("Er is geen Auto geselecteerd!");
                             }
                             else
                             {
-                                if(string.IsNullOrEmpty(cbxWerkplaats.Text))
+                                if (string.IsNullOrEmpty(cbxWerkplaats.Text))
                                 {
                                     MessageBox.Show("Er is geen Werkplaats geselecteerd!");
                                 }
                                 else
                                 {
-                                    // Class aanspreken voor opslaan, TODO: Class & functie maken   
+                                    onderhoud HetOnderhoud = new onderhoud();
+                                    var date = dpUitvoerdatum.SelectedDate.Value.Date;
+
+
+                                    string sBeurtnaam = tbxbeurtnaam.Text;
+                                    int dprijs = Int32.Parse(tbxprijs.Text);
+                                    string sOmschrijving = tbxomschrijving.Text;
+                                    int TextboxAuto = Convert.ToInt32(cbxauto.SelectedValue);
+                                    int TextboxWerk = Convert.ToInt32(cbxWerkplaats.SelectedValue);
+
+                                    //Object persoon aanmaken
+                                    HetOnderhoud.beurtnaam = sBeurtnaam;
+                                    HetOnderhoud.kosten = dprijs;
+                                    HetOnderhoud.omschrijving = sOmschrijving;
+                                    HetOnderhoud.uitvoerdatum = date;
+                                    HetOnderhoud.autoId = TextboxAuto;
+                                    HetOnderhoud.werkplaatsId = TextboxWerk;
+
+                                    //Insert in de wachtrij plaatsen
+                                    db.onderhouds.InsertOnSubmit(HetOnderhoud);
+                                    //Daadwerkelijk doorvoeren naar de database
+                                    db.SubmitChanges();
+                                    refreshgrid();
+                                    
+
+
                                 }
                             }
                         }
+                       
                     }
                 }
+
             }
         }
+        private void btnWijzigBeurt_Click(object sender, RoutedEventArgs e)
+        {
+            onderhoud HetOnderhoud = (onderhoud)dgBeurten.SelectedItem;
+            wBeurt WijzigBeurt = new wBeurt(HetOnderhoud, db);
+            WijzigBeurt.Show();
+            refreshgrid();
+        }
+
+        private void btnVerwijderen_Click(object sender, RoutedEventArgs e)
+        {
+            onderhoud HetOnderhoud = (onderhoud)dgBeurten.SelectedItem;
+
+            MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Weet je het zeker?", "Bevestig Verwijder", System.Windows.MessageBoxButton.YesNo);
+            if (messageBoxResult == MessageBoxResult.Yes)
+            {
+                db.onderhouds.DeleteOnSubmit(HetOnderhoud);
+                db.SubmitChanges();
+                refreshgrid();
+            }
+        }
+
         private void DpUitvoerdatum_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
         {
             DateTime datum = Convert.ToDateTime(dpUitvoerdatum.SelectedDate);
-            cbxauto.ItemsSource =  leaseC.autoSelect(datum);
 
         }
 
@@ -96,5 +148,6 @@ namespace ROCit_P31
                 tbxprijs.Text = tbxprijs.Text.Remove(tbxprijs.Text.Length - 1);
             }
         }
+
     }
 }
